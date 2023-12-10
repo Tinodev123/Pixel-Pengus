6697863a0563c20a17d64ccaf1c02a3c724d7d38
Tinodev123 <92589571+Tinodev123@users.noreply.github.com>
2023-12-10 18:39:58 +0100

    Create ConnectButton.js

---
+++ src/views/ConnectButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ConnectButton = () => {
+  const connectWithPhantom = async () => {
+    if (window.solana && window.solana.isPhantom) {
+      try {
+        // Connect to Phantom Wallet
+        await window.solana.connect();
+        console.log('Connected to Phantom Wallet!');
+      } catch (error) {
+        console.error('Error connecting to Phantom Wallet:', error.message);
+      }
+    } else {
+      console.error('Phantom Wallet not detected. Make sure it is installed.');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={connectWithPhantom}>Connect with Phantom Wallet</button>
+    </div>
+  );
+};
+
+export default ConnectButton;
